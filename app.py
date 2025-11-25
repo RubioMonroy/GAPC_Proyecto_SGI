3183070c205e4ceaf66ca2795a02938289ac8bb3
RubioMonroy <00064315@uca.edu.sv>
2025-11-25 01:42:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 import streamlit as st
 from modulos.asistencia import mostrar_asistencia  # Importamos la función mostrar_asistencia del módulo asistencia
+from modulos.login import login
 
 # Llamamos a la función mostrar_asistencia para mostrar el mensaje en la app
 mostrar_asistencia()
+login()
