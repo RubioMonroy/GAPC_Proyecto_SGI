e01006390a16c135d6a66ea61f3da31598855e9e
RubioMonroy <00064315@uca.edu.sv>
2025-11-25 05:30:41 -0600

    Delete modulos/asistencia.py

--- modulos/asistencia.py
+++
@@ -1,9 +0,0 @@
-# modulos/asistencia.py
-
-import streamlit as st
-
-def mostrar_asistencia():
-    # Aquí se muestra "Hola Mundo"
-    st.title("Hola Mundo")
-    st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
-
