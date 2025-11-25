ca2d170d3e9400f73c4c954bc6c61ecd04ff6355
RubioMonroy <00064315@uca.edu.sv>
2025-11-25 00:55:26 -0600

    Fix import comment formatting in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 import streamlit as st
-from modulos.asistencia import mostrar_asistencia # Importamos la función mostrar_venta del módulo asistencia
+from modulos.asistencia import mostrar_asistencia  # Importamos la función mostrar_asistencia del módulo asistencia
 
 # Llamamos a la función mostrar_asistencia para mostrar el mensaje en la app
 mostrar_asistencia()
